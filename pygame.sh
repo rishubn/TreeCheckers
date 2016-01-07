515d96c74a9e5cb742e23bf70072657901d2a803
Rishub Nagpal <rnagpal@d3engine.com>
2016-01-06 20:33:01 -0500

    Delete pygame.sh

--- pygame.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-sudo apt-get install mercurial python3-dev python3-numpy libav-tools \
-    libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
-    libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev -y
-hg clone https://bitbucket.org/pygame/pygame
-cd pygame
-python3 setup.py build
-sudo python3 setup.py install
