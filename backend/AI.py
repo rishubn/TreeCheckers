2779659b3c3f5ea81d015bd978aa29d5d33665a0
Vaz <fccinelli@wpi.edu>
2016-03-25 16:13:10 -0400

    Fixed minimax (I think. Needs more tests). Added lots of nice comments too.

--- backend/AI.py
+++ backend/AI.py
@@ -13,7 +13,7 @@ def generatePriorityMap(root,depth=0,priorityMap={}):
     return priorityMap
 
 # @RN March 15 2016
-# Generates possible moves for a root node and all it's children
+# Generates possible moves for a root node and all its children
 # Returns map with node.ID as key and a list of tuples of possible moves
 # segmented by an angle
 # Ex: {1: [
@@ -38,25 +38,35 @@ def generateMoveMap(root, radius, degree=10,radian=False,segments=0,moveMap={}):
             generateMoveMap(child,radius,degree,radian,segments,moveMap)
     return moveMap
 
-
-
-
+#@FCC March 2016
+#TODO: generalize this to any number of players
+#board is a list of nodes, depth is an integer, degree is a number, moveradius is a number, and pnum is an integer
 #returns the value of a move
-#these nodes are NOT game nodes, they are gamestates.
-def minimax(board, depth, pnum):
+#these nodes are NOT game nodes, they are gamestates. Their children are subsequent game states.
+def minimax(board, depth, degree, moveradius, pnum):
     if depth == 0 or len(node.children) == 0:
         return heuristic(board)
 
     if pnum == 0: #max
+        moveMap = {}
+        for root in board:
+            moveMap.update(generateMoveMap(root = root, radius = moveradius))
+        
         bestvalue = sys.float_info.max
-        for move in moveMap: #probably wrong
-            value = minimax(move, depth - 1, 1)
-            bestvalue = max(value, bestvalue)
+        for nodeId in movemap:
+            for move in movemap[nodeId]:
+                value = minimax(applyMove(board, nodeId, move), depth - 1, 1)
+                bestvalue = max(value, bestvalue)
     else: #min
+        moveMap = {}
+        for root in board:
+            movemap.update(generateMoveMap(root = root, radius = moveradius))
+        
         bestvalue = sys.float_info.min
-        for move in moveMap: #ditto
-            value = minimax(move, depth - 1, 0)
-            bestvalue = min(bestvalue, value)
+        for nodeId in moveMap:
+            for move in movemap[nodeId]:
+                value = minimax(applyMove(board, nodeId, move), depth - 1, 0)
+                bestvalue = min(bestvalue, value)
     return value
 
 #bs is a board state (a list of roots)
@@ -75,17 +85,13 @@ def applyMove(bs, nodeId, location):
             node.y = location[1]
     return newBs
 
-
-
-
-
-
-#thebs is the current boar state
-def getOptimalMove(thebs, depth, pnum):
+#@FCC March 2016
+#thebs is the current board state, represented as a list of root nodes
+def getOptimalMove(thebs, depth, degree, moveradius, pnum):
     bestMove = None
     bestMoveValue = sys.float_info.min
     for move in moveMap: #might be wrong
-        value = minimax(thebs, depth, pnum)
+        value = minimax(thebs, depth, degree, moveradius, pnum)
         if value >= bestMoveValue:
             bestMoveValue = value
             bestMove = move
